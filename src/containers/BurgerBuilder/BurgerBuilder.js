1c36b60b0d779936a49ebda149ec249fa110f0d7
Pyotr Drobyshev <pa.drobyshev@gmail.com>
2020-05-18 17:56:15 +0600

    added handlers for Button.js in OrderSummary.js modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	};
 
+	purchaseContinueHandler = () => {
+		alert('You continue!');
+	};
+
 	render() {
 		const disableInfo = {
 			...this.state.ingredients,
@@ -87,9 +91,15 @@ class BurgerBuilder extends Component {
 		return (
 			<Aux>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} />
+					<OrderSummary
+						ingredients={this.state.ingredients}
+						purchaseCanceled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
+					/>
 				</Modal>
+
 				<Burger ingredients={this.state.ingredients} />
+
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
