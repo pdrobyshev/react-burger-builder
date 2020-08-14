19378a655ab1778c0254916364e65a81067968b3
pdrobyshev <pdrobyshev@wooppay.com>
2020-08-14 22:32:21 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import CheckOut from './containers/CheckOut/CheckOut';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
 			<Layout>
-				<BurgerBuilder />
+				<Switch>
+					<Route path="/checkout" component={CheckOut} />
+					<Route path="/" exact component={BurgerBuilder} />
+				</Switch>a
 			</Layout>
 		);
 	}
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,6 +26,8 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
+		console.log(this.props);
+
 		axios
 			.get('https://react-my-burger-d3254.firebaseio.com/ingredients.json')
 			.then((response) => {
@@ -88,31 +90,31 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHandler = () => {
 		// alert('You continue!');
-		this.setState({ loading: true });
-
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Max Max',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41351',
-					country: 'Germany',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		};
-
-		axios
-			.post('/orders.json', order)
-			.then((response) => {
-				this.setState({ loading: false, purchasing: false });
-			})
-			.catch((error) => {
-				this.setState({ loading: false, purchasing: false });
-			});
+		// this.setState({ loading: true });
+		//
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Max Max',
+		// 		address: {
+		// 			street: 'Teststreet 1',
+		// 			zipCode: '41351',
+		// 			country: 'Germany',
+		// 		},
+		// 		email: 'test@test.com',
+		// 	},
+		// 	deliveryMethod: 'fastest',
+		// };
+		//
+		// axios
+		// 	.post('/orders.json', order)
+		// 	.then((response) => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	})
+		// 	.catch((error) => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	});
 	};
 
 	render() {
