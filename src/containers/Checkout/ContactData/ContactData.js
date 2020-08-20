a77e618eb9e28504be8b7c1d5645fca0b72c43c5
pdrobyshev <pdrobyshev@wooppay.com>
2020-08-20 21:08:20 +0600

    created Input.js component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
@@ -48,10 +49,10 @@ class ContactData extends Component {
 	render() {
 		let form = (
 			<form action="/">
-				<input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-				<input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-				<input className={classes.Input} type="text" name="street" placeholder="Street" />
-				<input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+				<Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+				<Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+				<Input inputtype="input" type="text" name="street" placeholder="Street" />
+				<Input inputtype="input" type="text" name="postal" placeholder="Postal code" />
 
 				<Button btnType="Success" clicked={this.orderHandler}>
 					ORDER
