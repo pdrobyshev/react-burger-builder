fe2d55427d91888d5b81d8feea96145e130c09c8
pdrobyshev <pdrobyshev@wooppay.com>
2021-02-09 20:11:06 +0600

    store user data in local storage try to auto log in on page refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -9,6 +11,10 @@ import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
+	componentDidMount() {
+		this.props.onTryAutoSignup();
+	}
+
 	render() {
 		return (
 			<Layout>
@@ -24,4 +30,10 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onTryAutoSignup: () => dispatch(actions.authCheckState()),
+	};
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
