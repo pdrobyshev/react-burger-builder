fa644628db7a7e04ca9c4c20c4258a1fbb9028d1
pdrobyshev <pdrobyshev@wooppay.com>
2021-02-11 14:58:27 +0600

    outsource checkValidity method fix css

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
 	state = {
@@ -75,6 +76,7 @@ class ContactData extends Component {
 				value: '',
 				validation: {
 					required: true,
+					isEmail: true,
 				},
 				valid: false,
 				touched: false,
@@ -95,27 +97,6 @@ class ContactData extends Component {
 		formIsValid: false,
 	};
 
-	checkValidity(value, rules) {
-		let isValid = true;
-		if (!rules) {
-			return true;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxlength) {
-			isValid = value.length <= rules.maxlength && isValid;
-		}
-
-		return isValid;
-	}
-
 	orderHandler = (event) => {
 		event.preventDefault();
 
@@ -135,25 +116,20 @@ class ContactData extends Component {
 	};
 
 	inputChangedHandler = (event, inputIdentifier) => {
-		const updatedOrderForm = {
-			...this.state.orderForm,
-		};
-		const updatedFormElement = {
-			...updatedOrderForm[inputIdentifier],
-		};
-		updatedFormElement.value = event.target.value;
-		updatedFormElement.valid = this.checkValidity(
-			updatedFormElement.value,
-			updatedFormElement.validation
-		);
-		updatedFormElement.touched = true;
-		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+			value: event.target.value,
+			valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+			touched: true,
+		});
+
+		const updatedOrderForm = updateObject(this.state.orderForm, {
+			[inputIdentifier]: updatedFormElement,
+		});
 
 		let formIsValid = true;
 		for (let inputIdentifier in updatedOrderForm) {
 			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
 		}
-
 		this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
 	};
 
@@ -215,7 +191,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
