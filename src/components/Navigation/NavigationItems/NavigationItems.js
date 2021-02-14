c36786237fd1232dc676048983df498cca077c15
pdrobyshev <pdrobyshev@wooppay.com>
2021-02-14 12:44:01 +0600

    first tests

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,10 +8,10 @@ const navigationItems = (props) => (
 			Burger Builder
 		</NavigationItem>
 		{props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
-		{!props.isAuthenticated ? (
-			<NavigationItem link="/auth">Authenticate</NavigationItem>
-		) : (
+		{props.isAuthenticated ? (
 			<NavigationItem link="/logout">Logout</NavigationItem>
+		) : (
+			<NavigationItem link="/auth">Authenticate</NavigationItem>
 		)}
 	</ul>
 );
